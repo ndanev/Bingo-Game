aa1735a6f59b0b1b6ac0e5966055b8f36bb10976
Nemanja Danev <nemanja.danev.93@gmail.com>
2018-07-26 19:44:21 -0400

    comapred random numbers with numbers on the bingo ticket

--- list.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    var example = $('.example');
-        example.append('<div class="example-inner"></div>');
-    var inner = $('.example-inner');
-        inner.append('<div class="box"></box>');
-});
--- main.js
+++ main.js
@@ -1,45 +1,57 @@
 var ticket = [
-    "*", 12, "*", 25, 34, 10, 24, "*", "*",
-    55, "*", 67, 87, 19, "*", "*", 21, 7,
-    1, 8, "*", "*", 33, 17, 47, "*", 25
-]
-
-
-// choose list
-function changeStyle1() {
-    // change style
-    var change = document.getElementById('grid1');
-    change.style.border = "3px dashed rgb(255, 209, 71)";
-
-    for (i = 0; i < ticket.length; i++) {
-       document.getElementById('item' + (i + 1)).innerText = ticket[i];
-    }
-    
-    
-};
-
-//function changeStyle2() {
-  //  var change = document.getElementById('grid2');
-    //change.style.border = "3px dashed rgb(255, 209, 71)";
-    
-//};
-
-//function changeStyle3() {
-  //  var change = document.getElementById('grid3');
-    //change.style.border = "3px dashed rgb(255, 209, 71)";
-    
-//};
-
-
-
-
-// Pulling out numbers
-function numbers(){
-    var numb = document.getElementById('demo');
-    numb.innerText= Math.floor(Math.random() * 90);
-    setTimeout(numbers,5000);
-}
-
+    // "*", 12, "*", 25, 34, 10, 24, "*", "*",
+    // 55, "*", 67, 87, 19, "*", "*", 21, 7,
+    // 1, 8, "*", "*", 33, 17, 47, "*", 25
+ ]
+ 
+ // choose ticket
+ function changeStyle1() {
+     // change style
+     var change = document.getElementById('grid1');
+     change.style.border = "3px dashed rgb(255, 209, 71)";
+ 
+     for (i = 0; i < 27; i++) {
+        document.getElementById('item' + (i + 1)).innerText = Math.floor(Math.random() * 90);
+     }
+     
+     
+ };
+ 
+ //function changeStyle2() {
+   //  var change = document.getElementById('grid2');
+     //change.style.border = "3px dashed rgb(255, 209, 71)";
+     
+ //};
+ 
+ //function changeStyle3() {
+   //  var change = document.getElementById('grid3');
+     //change.style.border = "3px dashed rgb(255, 209, 71)";
+     
+ //};
+ 
+ 
+ 
+ 
+ // Pulling out numbers
+ function numbers(){
+     var numb = document.getElementById('demo');
+     numb.innerText= Math.floor(Math.random() * 90);
+ 
+     // check for winning number
+     for (i = 0; i < 27; i++) {
+         if (document.getElementById('item' + (i + 1)).innerText === numb.innerText) {
+             document.getElementById('item' + (i + 1)).style.backgroundColor = "rgb(0, 255, 128)";
+         }
+ 
+        
+      }
+ 
+ 
+     setTimeout(numbers,3000);
+ 
+ }
+ 
+ 
 
 
 
