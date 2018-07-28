391ea83746f4b7eb2b4a9e8d313a7e80233cea5d
Nemanja Danev <nemanja.danev.93@gmail.com>
2018-07-28 14:09:30 -0400

    add if statemant for 0 number

--- main.js
+++ main.js
@@ -1,42 +1,51 @@
-var ticket = [
-    // "*", 12, "*", 25, 34, 10, 24, "*", "*",
-    // 55, "*", 67, 87, 19, "*", "*", 21, 7,
-    // 1, 8, "*", "*", 33, 17, 47, "*", 25
- ]
- 
- // choose ticket
- function changeStyle1() {
-     // change style
-     var change = document.getElementById('grid1');
-     change.style.border = "3px dashed rgb(255, 255, 81)";
- 
-     for (i = 0; i < 27; i++) {
-        document.getElementById('item' + (i + 1)).innerText = Math.floor(Math.random() * 90);
-     }
-     
-     
- };
- 
- // Pulling out numbers
- function numbers(){
-     var numb = document.getElementById('demo');
-     numb.innerText= Math.floor(Math.random() * 90);
- 
-     // check for winning number
-     for (i = 0; i < 27; i++) {
-         if (document.getElementById('item' + (i + 1)).innerText === numb.innerText) {
-             document.getElementById('item' + (i + 1)).style.backgroundColor = "rgb(0, 255, 128)";
-         }
- 
+var ticket = [];
+
+// choose ticket
+function changeStyle1() {
+    // change style
+    var change = document.getElementById('grid1');
+    change.style.border = "3px dashed rgb(255, 255, 81)";
+
+
+    for (i = 0; i < 27; i++) {
         
-      }
- 
- 
-     setTimeout(numbers,3000);
- 
- }
- 
- 
+        document.getElementById('item' + (i + 1)).innerHTML = Math.floor(Math.random() * 90);
+        if (document.getElementById('item' + (i + 1)).innerHTML === "0") {
+
+            document.getElementById('item' + (i + 1)).innerHTML = "*";
+        
+        }
+
+
+    }
+
+    
+
+
+};
+
+
+
+// Pulling out numbers
+function numbers() {
+    var numb = document.getElementById('demo');
+    numb.innerText = Math.floor(Math.random() * 90);
+
+    // check for winning number
+    for (i = 0; i < 27; i++) {
+        if (document.getElementById('item' + (i + 1)).innerText === numb.innerText) {
+            document.getElementById('item' + (i + 1)).style.backgroundColor = "rgb(0, 255, 128)";
+        }
+
+
+    }
+
+
+    setTimeout(numbers, 3000);
+
+};
+
+
 
 
 
