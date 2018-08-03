b58e99cf907beedc1d5dff1d406b92c274172a98
Nemanja Danev <nemanja.danev.93@gmail.com>
2018-08-02 20:09:51 -0400

    put numbers in every separate row

--- main.js
+++ main.js
@@ -1,21 +1,17 @@
 var ticket = [];
 
-function nextNumber () {
-    var n =  Math.floor(Math.random() * 90);
+function nextNumber() {
+    var n = Math.floor(Math.random() * 90);
     return n;
-} 
+}
 
 function showTicket() {
     for (i = 0; i < 27; i++) {
-
-        // change 0 to *
         if (ticket[i] === 0) {
             document.getElementById('item' + (i + 1)).innerHTML = "*";
-           
         } else {
             document.getElementById('item' + (i + 1)).innerHTML = ticket[i];
         }
-
     }
     var change = document.getElementById('grid1');
     change.style.border = "3px dashed rgb(255, 255, 81)";
@@ -24,25 +20,64 @@ function showTicket() {
 
 // Ticket Generator
 function generateTicket() {
-    var counter = 0
 
-    for (i = 0; i < 27; i++) {
+    var countNum1 = 0;
+    
+    // first row
+    for (i = 0; i < 9; i++) {
+        var counter = 0;
         var m = nextNumber();
         if (ticket.includes(m)) {
             m = 0
         }
-        if (m!= 0) {
+        if (m != 0) {
             counter++
+            countNum1++
+            if (countNum1 > 5) {
+                m = 0
+            }
         }
-        if (counter > 15) {
-            m = 0
-        }
-        
         ticket[i] = m;
+
     }
 
+    var countNum2 = 0;
 
+    // second row
+    for (j = 9; j < 18; j++) {
+        var counter1 = 0;
+        var a = nextNumber();
+        if (ticket.includes(a)) {
+            a = 0
+        }
+        if (a != 0) {
+            counter1++
+            countNum2++
+            if (countNum2 > 5) {
+                a = 0
+            }
+        }
+         ticket[j] = a;
+    }
 
+    var countNum3 = 0;
+
+    // third row
+    for (k = 18; k < 27; k++) {
+        var counter2 = 0;
+        var b = nextNumber();
+        if (ticket.includes(b)) {
+            b = 0
+        }
+        if (b != 0) {
+            counter2++
+            countNum3++
+            if (countNum3 > 5) {
+                b = 0
+            }
+        }
+        ticket[k] = b
+    }
 
 };
 
@@ -50,7 +85,7 @@ function generateTicket() {
 
 
 // Pulling out numbers
-function numbers() {
+function startGame() {
     var numb = document.getElementById('demo');
     numb.innerText = Math.floor(Math.random() * 90);
 
@@ -59,13 +94,17 @@ function numbers() {
         if (document.getElementById('item' + (i + 1)).innerText === numb.innerText) {
             document.getElementById('item' + (i + 1)).style.backgroundColor = "rgb(0, 255, 128)";
         }
-
-
     }
-
-    setTimeout(numbers, 3000);
-
+    setTimeout(startGame, 3000);
 };
+
+// animate
+jQuery(document).ready(function ($) {
+    $('#ball').animate({
+        width: 500,
+        height: 500
+    }, 2000)
+});
 
 
 
